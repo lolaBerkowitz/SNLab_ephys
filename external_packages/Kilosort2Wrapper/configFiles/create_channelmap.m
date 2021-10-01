b2fd9faeaa257e0a0ecb5d14ffcdf1f1f26801ab
Laura Berkowitz <leb239@cornell.edu>
2021-10-01 14:23:14 -0400

    pulls info from xml instead of as inputs, removed cd to basepath

--- external_packages/Kilosort2Wrapper/configFiles/create_channelmap.m
+++ external_packages/Kilosort2Wrapper/configFiles/create_channelmap.m
@@ -4,6 +4,9 @@ function create_channelmap(basepath,varargin)
 %  kilosort 1 wrapper. Modified to not use channels that user set as 'skip'
 %  in the .xml file. Modified further to accomodate probe geometry. 
 %
+%  Assumes xml file is named after raw data folder basename in line with 
+%  buzcode data formatting standards https://github.com/buzsakilab/buzcode/wiki/Data-Formatting-Standards
+%
 %  Default map is for neuronexus A1x64-Poly2-6mm-23s-160. otherwise, add
 %  xcoords/ycoords,Nshanks for given probe. 
 %
@@ -13,38 +16,35 @@ function create_channelmap(basepath,varargin)
 p = inputParser; 
 p.addParameter('xcoords',[6,19,repmat([0,30],1,31)]');
 p.addParameter('ycoords', [linspace(0,1449.0,64)]');
-p.addParameter('Nshanks',1)
-p.addParameter('fs',30000)
+% p.addParameter('Nshanks',1)
+% p.addParameter('fs',30000)
 p.parse(varargin{:});
-if ~exist('basepath','var')
-    basepath = cd;
-end
 
-d   = dir('*.xml');
-par = LoadXml(fullfile(basepath,d(1).name));
-
+% unpack coordinates for channels 
 xcoords = p.Results.xcoords;
 ycoords = p.Results.ycoords;
+
+% load basename.xml
+[~,basename] = fileparts(basepath);
+d   = dir([basepath,filesep,basename,'.xml']);
+par = LoadXml(fullfile(basepath,d.name));
+
+% Get nshanks from electrode groups or anatomical groups
 if ~isfield(par,'nElecGps')
-    warning('No Electrode/Spike Groups found in xml.  Using Anatomy Groups instead.')
-    tgroups = par.ElecGp;
-    ngroups = length(tgroups);
+    Nshanks = length(par.ElecGp);
 else
-    t = par.AnatGrps;
-    ngroups = length(par.AnatGrps);
-    for g = 1:ngroups
-        tgroups{g} = par.AnatGrps(g).Channels;
-    end
+    warning('No Electrode/Spike Groups found in xml.  Using Anatomy Groups instead.')
+    Nshanks = length(par.AnatGrps);
 end
 
-% Unpack to save
-Nchannels = length(xcoords);
-Nshanks = p.Results.Nshanks;
-fs = p.Results.fs;
+% Pull Nchannels and sample rate from xml
+Nchannels = par.nChannels;
+fs = par.SampleRate; 
 
 kcoords = (reshape(repmat(1:Nshanks, Nchannels/Nshanks, 1), Nchannels, 1));
 
 connected   = true(Nchannels, 1); 
+
 %getting channels labeled as a skip to set as disconnected
 %with the code below I assume your anatomical group is the same as your
 %spike group, so make sure it is if you want to skip channels correctly
