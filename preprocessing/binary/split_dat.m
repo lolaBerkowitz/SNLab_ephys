dd6ed23f61ce93a9bd02d306b875f96c3d296023
Laura Berkowitz <leb239@cornell.edu>
2022-05-10 14:45:30 -0400

    small changes to process functions
    removed input that was not used, and added lines to address sessions with empty digitalin

--- preprocessing/binary/split_dat.m
+++ preprocessing/binary/split_dat.m
@@ -37,15 +37,18 @@ function split_dat(data_path,save_path, subject_order,varargin)
 
 p = inputParser;
 addParameter(p,'digitalin_order',[2,3,4,5],@isnumeric) %snlab rig wiring for events
-addParameter(p,'input_name',{'video','A','B','C','D'},@ischar) %snlab rig wiring
+addParameter(p,'input_name',{'video','A','B','C','D'},@iscell) %snlab rig wiring
 addParameter(p,'trim_dat',false,@islogical) %snlab rig wiring
+addParameter(p,'port_id',{'Port A','Port B','Port C','Port D'},@iscell) %ports used in recording
 
 parse(p,varargin{:});
 
 digitalin_order = p.Results.digitalin_order;
 input_name = p.Results.input_name;
 trim_dat = p.Results.trim_dat;
+port_id = p.Results.port_id;
 
+% parse inputs based port_id
 
 % identify inputs that require processing -- order of inputs is associated
 % with port, so [0 0 1 1] would correspond to ports C and D, [1 0 0 0] port
@@ -72,6 +75,10 @@ digitalIn = process_digitalin(data_path,frequency_parameters.board_dig_in_sample
 % Pull start and end from digitalin channels that correspond to dat
 port = 1;
 for channel =  digitalin_order(subject_idx)
+    
+    if isempty([digitalIn.timestampsOn{:}])
+        continue
+    end
     %timestamp multiplied by sample rate
     trim_dat_epoch(port,1) = digitalIn.timestampsOn{1, channel}(1)*...
         (frequency_parameters.board_dig_in_sample_rate);
@@ -89,13 +96,13 @@ end
 
 % splits dat according to subject_order and saves to data_path
 if trim_dat
-    process_aux(data_path,aux_input_channels,frequency_parameters,subject_order,basepath,...
+    process_aux(data_path,aux_input_channels,port_id,subject_order,basepath,...
         'trim_dat_epoch',trim_dat_epoch);
-    process_amp(data_path,amplifier_channels,frequency_parameters,...
+    process_amp(data_path,amplifier_channels,port_id,...
         subject_order,basepath,'trim_dat_epoch',trim_dat_epoch);
 else
-    process_aux(data_path,aux_input_channels,frequency_parameters,subject_order,basepath);
-    process_amp(data_path,amplifier_channels,frequency_parameters,...
+    process_aux(data_path,aux_input_channels,port_id,subject_order,basepath);
+    process_amp(data_path,amplifier_channels,port_id,...
         subject_order,basepath);
     
 end
@@ -105,13 +112,16 @@ end
 vid_files = dir([data_path,filesep,'*.avi']);
 vids = {vid_files.name};
 % Order of folders should match port_order and digitalin_order
+
 digidx = 1;
 for port = find(~cellfun(@isempty,subject_order))
     
     % if video file with subid is present, move that to basepath
     if ~isempty(vids(contains({vid_files.name},subject_order{port})))
         file = vids(contains({vid_files.name},subject_order{port}));
-        movefile([data_path,filesep,file{1}],basepath{port})
+        for i = 1:length(file)
+            movefile([data_path,filesep,file{i}],basepath{port})
+        end
     end
     
     % make copy of rhd, and setting to basepath
@@ -134,7 +144,7 @@ end
 end
 
 % Local functions
-function process_aux(dat_path,aux_input_channels,frequency_parameters,subject_order,basepath,varargin)
+function process_aux(dat_path,aux_input_channels,port_id,subject_order,basepath,varargin)
 % processes intan auxillary file and splits into separate files indicated
 % by ports.
 % input:
@@ -155,10 +165,11 @@ parse(p,varargin{:});
 
 trim_dat_epoch = p.Results.trim_dat_epoch;
 
-
 % Check to see if files have been created
 % find ports to write
-write_port = unique({aux_input_channels.port_name});
+write_port = unique({aux_input_channels.port_name}); 
+subject_order = subject_order(contains({'Port A','Port B','Port C','Port D'},write_port));
+basepath = basepath(contains(port_id,write_port));
 basepath = basepath(~cellfun(@isempty,subject_order));
 write_port = write_port(~cellfun(@isempty,subject_order)); % write ports based on inputs
 
@@ -176,7 +187,7 @@ aux = memmapfile(contFile,'Format',{'uint16' [n_channels samples] 'mapped'});
 
 % create batches
 if isempty(trim_dat_epoch)
-    batch = ceil(linspace(0,samples,ceil(samples/frequency_parameters.aux_input_sample_rate/4)));
+    batch = ceil(linspace(0,samples,100));
 end
 % initialize new dat, channel index, and batch
 for port = 1:length(write_port)
@@ -198,21 +209,23 @@ else
 end
 
 % check size of amp compared to calcuated bytes
-for port = 1:length(basepath)
-    t = dir([basepath{port},filesep,'auxiliary.dat']);
-    samples = t.bytes/(3 * 2); %int16 = 2 bytes, default
-    calc_samples = round(trim_dat_epoch(port,2) - trim_dat_epoch(port,1));
-    if samples == calc_samples
-        disp([basepath{port},filesep,'auxiliary.dat ','size verified'])
-    else
-        disp([basepath{port},filesep,'auxiliary.dat ','has different size than epoch'])
+
+if ~isempty(trim_dat_epoch)
+    for port = 1:length(basepath)
+        t = dir([basepath{port},filesep,'auxiliary.dat']);
+        samples = t.bytes/(3 * 2); %int16 = 2 bytes, default
+        calc_samples = round(trim_dat_epoch(port,2) - trim_dat_epoch(port,1));
+        if samples == calc_samples
+            disp([basepath{port},filesep,'auxiliary.dat ','size verified'])
+        else
+            disp([basepath{port},filesep,'auxiliary.dat ','has different size than epoch'])
+        end
     end
 end
-
 clear aux
 end
 
-function process_amp(dat_path,amplifier_channels,frequency_parameters,subject_order,basepath,varargin)
+function process_amp(dat_path,amplifier_channels,port_id,subject_order,basepath,varargin)
 % processes intan amplifier file and splits into separate files indicated
 % by ports.
 % input:
@@ -239,10 +252,11 @@ parse(p,varargin{:});
 trim_dat_epoch = p.Results.trim_dat_epoch;
 
 % loop through ports
-write_port = unique({amplifier_channels.port_name});
+write_port = unique({amplifier_channels.port_name}); 
+subject_order = subject_order(contains(port_id,write_port));
+basepath = basepath(contains({'Port A','Port B','Port C','Port D'},write_port));
 basepath = basepath(~cellfun(@isempty,subject_order));
 write_port = write_port(~cellfun(@isempty,subject_order)); % write ports based on inputs
-
 % loop through ports
 remove_port_idx = [];
 for port = 1:length(write_port)
@@ -271,7 +285,7 @@ amp = memmapfile(contFile,'Format',{'int16' [n_channels, samples] 'mapped'});
 
 % create batches
 if isempty(trim_dat_epoch)
-    batch = ceil(linspace(0,samples,ceil(samples/frequency_parameters.amplifier_sample_rate/4)));
+    batch = ceil(linspace(0,samples,10000));
 end
 % initialize new dat, channel index, and batch
 for port = 1:length(write_port)
@@ -300,17 +314,18 @@ end
 fclose('all');
 
 % check size of amp compared to calcuated bytes
-for port = 1:length(basepath)
-    t = dir([basepath{port},filesep,'amplifier.dat']);
-    samples = t.bytes/(sum(idx{port}) * 2); %int16 = 2 bytes
-    calc_samples = round(trim_dat_epoch(port,2) - trim_dat_epoch(port,1));
-    if samples == calc_samples
-        disp([basepath{port},filesep,'amplifier.dat ','size verified'])
-    else
-        disp([basepath{port},filesep,'amplifier.dat ','has different size than epoch'])
+if ~isempty(trim_dat_epoch)
+    for port = 1:length(basepath)
+        t = dir([basepath{port},filesep,'amplifier.dat']);
+        samples = t.bytes/(sum(idx{port}) * 2); %int16 = 2 bytes
+        calc_samples = round(trim_dat_epoch(port,2) - trim_dat_epoch(port,1));
+        if samples == calc_samples
+            disp([basepath{port},filesep,'amplifier.dat ','size verified'])
+        else
+            disp([basepath{port},filesep,'amplifier.dat ','has different size than epoch'])
+        end
     end
 end
-
 clear amp
 end
 
