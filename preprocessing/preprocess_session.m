3de2359d1a94ead3412244e1f8419d8bd8745d75
Laura Berkowitz <leb239@cornell.edu>
2022-05-10 14:47:56 -0400

    flag for kilosort, and check_epochs
    added to allow for processing without spike sorting

--- preprocessing/preprocess_session.m
+++ preprocessing/preprocess_session.m
@@ -1,7 +1,7 @@
 
 function preprocess_session(basepath,varargin)
 % Function for processing electrophysiology data in the SNLab adapted from
-% preprocessSession.m ayalab/neurocode repo 
+% preprocessSession.m ayalab/neurocode repo
 %
 % Dependencies
 %   - external packages: neurocode,KS2Wrapper
@@ -29,13 +29,14 @@ addParameter(p,'getLFP',true,@islogical);
 addParameter(p,'getEMG',true,@islogical);
 addParameter(p,'stateScore',true,@islogical);
 addParameter(p,'DLC',false,@islogical);
+addParameter(p,'kilosort',false,@islogical);
 addParameter(p,'removeNoise',false,@islogical);
 addParameter(p,'ssd_path','C:\kilo_temp',@ischar);    % Path to SSD disk.
 addParameter(p,'lfp_fs',1250,@isnumeric);
 addParameter(p,'specialChannels',[30,59,17],@isnumeric) % default for ASYpoly2A64
-addParameter(p,'acquisition_event_flag',false,@islogical) % if you have start and stop recording 
+addParameter(p,'acquisition_event_flag',false,@islogical) % if you have start and stop recording
 % events else default uses first and last event as indicies for start and stop of recording
-addParameter(p,'check_epochs',true,@islogical) % fixes bouncy events
+addParameter(p,'check_epochs',false,@islogical) % fixes bouncy events
 
 parse(p,varargin{:});
 
@@ -47,6 +48,7 @@ getAcceleration = p.Results.getAcceleration;
 getLFP = p.Results.getLFP;
 getEMG = p.Results.getEMG;
 concatenateDat = p.Results.concatenateDat;
+kilosort = p.Results.kilosort;
 
 % cleanArtifacts = p.Results.cleanArtifacts;
 stateScore = p.Results.stateScore;
@@ -61,6 +63,13 @@ check_epochs = p.Results.check_epochs;
 
 % Set basename from folder
 basename = basenameFromBasepath(basepath);
+
+% If non-contiguous recordings were taken, merge the dat files into one
+% session. Default is false.
+if concatenateDat
+    concatenateDats(basepath,0,1);
+end
+
 
 % rename amplifier.dat to basename.dat
 if ~isempty(dir([basepath,filesep,'amplifier.dat']))
@@ -79,29 +88,22 @@ if ~isempty(dir([basepath,filesep,'amplifier.xml']))
 end
 
 % Create SessionInfo
-session = sessionTemplate(basepath,'showGUI',true);
-
-% If non-contiguous recordings were taken, merge the dat files into one
-% session. Default is false. 
-if concatenateDat
-    concatenateDats(basepath,0,1);
-end
-
-% Process additional inputs 
+session = sessionTemplate(basepath,'showGUI',false);
+% Process additional inputs
 
 % Analog inputs
-    % check the two different fucntions for delaing with analog inputs and proably rename them
-if analogInputs 
+% check the two different fucntions for delaing with analog inputs and proably rename them
+if analogInputs
     if  ~isempty(analogChannels)
         analogInp = computeAnalogInputs('analogCh',analogChannels,'saveMat',true,'fs',session.extracellular.sr);
     else
-        analogInp = computeAnalogInputs('analogCh',[],'saveMat',true,'fs',session.extracellular.sr); 
+        analogInp = computeAnalogInputs('analogCh',[],'saveMat',true,'fs',session.extracellular.sr);
     end
     
-% analog pulses ... 
-    [pulses] = getAnalogPulses('samplingRate',session.extracellular.sr);     
+    % analog pulses ...
+    [pulses] = getAnalogPulses('samplingRate',session.extracellular.sr);
 end
-   
+
 % Digital inputs
 if digitalInputs
     if ~isempty(digitalChannels)
@@ -145,7 +147,7 @@ else
 end
 save(fullfile(basepath,[basename, '.session.mat']),'session');
 
-%  annotate session epochs 
+%  annotate session epochs
 if check_epochs
     gui_session
 end
@@ -153,7 +155,7 @@ end
 
 % Auxilary input
 if getAcceleration
-    accel = computeIntanAccel('saveMat',true); 
+    accel = computeIntanAccel('saveMat',true);
 end
 
 % remove noise from data for cleaner spike sorting
@@ -162,62 +164,63 @@ if removeNoise
 end
 
 if getLFP
-    % create downsampled lfp low-pass filtered lfp file 
+    % create downsampled lfp low-pass filtered lfp file
     LFPfromDat(basepath,'outFs',lfp_fs,'useGPU',true);
 end
 
-% Calcuate estimated emg 
+% Calcuate estimated emg
 if getEMG
     chInfo = hackInfo('basepath',basepath);
     EMGFromLFP = getEMGFromLFP(basepath,'overwrite',false,'noPrompts',true,...
-                           'saveMat',true,'chInfo',chInfo,'specialChannels',specialChannels);
+        'saveMat',true,'chInfo',chInfo,'specialChannels',specialChannels);
 end
 
 % Get brain states
-% an automatic way of flaging bad channels is needed 
-if stateScore 
-    try 
+% an automatic way of flaging bad channels is needed
+if stateScore
+    try
         if exist('pulses','var')
             SleepScoreMaster(basepath,'noPrompts',true,'ignoretime',pulses.intsPeriods); % try to sleep score
-            thetaEpochs(basepath); 
+            thetaEpochs(basepath);
         else
             SleepScoreMaster(basepath,'noPrompts',true); % takes lfp in base 0
-            thetaEpochs(basepath); 
+            thetaEpochs(basepath);
         end
     catch
         disp('Problem with SleepScore skiping...');
     end
 end
 
-% For Kilosort: create channelmap
-create_channelmap(basepath)
-
-% creating a folder on the ssd for chanmap,dat, and xml
-ssd_folder = fullfile(ssd_path, basename);
-mkdir(ssd_folder);
-
-% Copy chanmap,basename.dat, and xml
-disp('Copying basename.dat, basename.xml, and channelmap to ssd')
-
-disp('Saving dat file to ssd')
-command = ['robocopy "',basepath,'" ',ssd_folder,' ',basename,'.dat'];
-system(command);
-
-disp('Saving xml to ssd')
-command = ['robocopy "',basepath,'" ',ssd_folder,' ',basename,'.xml'];
-system(command);
-
-disp('Saving channel_map to ssd')
-command = ['robocopy "',basepath,'" ',ssd_folder,' chanMap.mat'];
-system(command);
-
-% Spike sort using kilosort 1 (data on ssd)
-run_ks1(basepath,ssd_folder)
-
+if kilosort
+    % For Kilosort: create channelmap
+    create_channelmap(basepath)
+    
+    % creating a folder on the ssd for chanmap,dat, and xml
+    ssd_folder = fullfile(ssd_path, basename);
+    mkdir(ssd_folder);
+    
+    % Copy chanmap,basename.dat, and xml
+    disp('Copying basename.dat, basename.xml, and channelmap to ssd')
+    
+    disp('Saving dat file to ssd')
+    command = ['robocopy "',basepath,'" ',ssd_folder,' ',basename,'.dat'];
+    system(command);
+    
+    disp('Saving xml to ssd')
+    command = ['robocopy "',basepath,'" ',ssd_folder,' ',basename,'.xml'];
+    system(command);
+    
+    disp('Saving channel_map to ssd')
+    command = ['robocopy "',basepath,'" ',ssd_folder,' chanMap.mat'];
+    system(command);
+    
+    % Spike sort using kilosort 1 (data on ssd)
+    run_ks1(basepath,ssd_folder)
+end
 % Get tracking positions - TO FIX
 if DLC
     disp('DLC loader is in development LB 03/22')
-%     getSessionTracking('optitrack',false);
+    %     getSessionTracking('optitrack',false);
 end
 
 end
