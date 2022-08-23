16822870739372dea6c582b26019b2e9e607ae7f
Laura Berkowitz <leb239@cornell.edu>
2022-08-23 17:08:11 -0400

    updated inputs to handle csv and paths, will run functions in get_sessions(new function name tbd), removed anonymous function handles
    still in testing - currently only running remedy_check == 'tracking'

--- preprocessing/find_and_remedy_preprocessing.m
+++ preprocessing/find_and_remedy_preprocessing.m
@@ -1,43 +1,53 @@
-% batch_preprocessing uses session_status to preprocess sessions with missing
+
+function find_and_remedy_preprocessing(data_folder,varargin)
+% find_and_remedy_preprocessing uses session_status to preprocess sessions with missing
 % analyses. Analyses carried out relative to completion of dependencies.
 % For instance, sleep states can only be run if the .lfp file is present in
 % the data bath.
+%
+% input:
+%   data_folder: path to parent directory containing basepaths or csv of
+%   basepaths with column basepath.
 
-data = 'Y:\laura_berkowitz\app_ps1_ephys\data';
-remedy_check = 'tracking';
+p = inputParser;
+p.addParameter('remedy_check','all')
 
-folders = dir(data);
-folders = folders(~ismember({folders.name},{'.','..'}),:); % remove ., ..
-folders = folders(~ismember({folders.name},'to_split'));
+p.parse(varargin{:})
+remedy_check = p.Results.remedy_check;
 
-for i = 1:length({folders.name})
-    data_folder = fullfile(folders(i).folder,folders(i).name);
-    
-    % first run a check
-    check_processing_status(data_folder)
-    
-    % load check
-    check = dir(fullfile(data_folder,'session_check*.csv'));
-    status = readtable(fullfile(data_folder,check.name));
-    
-    % remedy processing 
-    [basepaths,func] = get_sessions(status,remedy_check);
-    
-    for sess = 1:length(basepaths)
-        % run function 
-        func('basepath',basepath) 
-    end
-    
+% handle input as csv of basepaths or directory
+if contains(data_folder,'.csv')
+    df = readtable(data_folder);
+else
+    df = compile_sessions(data_folder);
 end
 
 
+% first run a check
+check_processing_status(data_folder)
+data_folder = fileparts(data_folder);
+
+% load check
+check = dir(fullfile(data_folder,'session_check*.csv'));
+status = readtable(fullfile(data_folder,check.name));
+
+%% remedy processing
+get_sessions(status,remedy_check);
+
+
+end
 
 function [basepaths,f] = get_sessions(status,remedy_check)
 
 switch remedy_check
     case 'tracking'
-        basepaths = status.basepath(status.tracking_dlc == 1 & status.tracking_animalBehavior == 0);
-        f = @process_tracking;
+        basepaths = status.basepath(status.tracking_dlc == 1 & (status.tracking_animalBehavior == 0 | ...
+            status.tracking_restrictxy == 0 | status.tracking_mazeCoords == 0 | ...
+            status.tracking_sessionBehavioralTracking == 0));
+        for i = 1:length(basepaths)
+            disp(['Processing : ',basepaths{i}])
+            process_tracking(basepaths{i})
+        end
     case 'kilosort'
         basepaths = status.basepath(status.kilosort == 0);
         f.a = @create_channelmap;
@@ -48,7 +58,7 @@ switch remedy_check
         f.b = @thetaEpochs;
     case 'lfp'
         basepaths = status.basepath(status.lfp == 0);
-        f = @preprocess_session; % if no lfp, preprocessing has likely not been run. 
+        f = @preprocess_session; % if no lfp, preprocessing has likely not been run.
     case 'cell_metrics'
         basepaths = status.basepath(status.kilosort == 1 & status.phyRez == 1 & status.cell_metrics == 0);
         f = @ProcessCellMetrics;
@@ -58,4 +68,3 @@ switch remedy_check
 end
 
 end
-
