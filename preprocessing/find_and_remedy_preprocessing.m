6fbe91c5d8710eeefb35aec5f4520e59c412f54c
Laura Berkowitz <leb239@cornell.edu>
2022-08-25 17:15:11 -0400

    removed maze coords from tracking and started added functions to other checks

--- preprocessing/find_and_remedy_preprocessing.m
+++ preprocessing/find_and_remedy_preprocessing.m
@@ -42,7 +42,7 @@ function [basepaths,f] = get_sessions(status,remedy_check)
 switch remedy_check
     case 'tracking'
         basepaths = status.basepath(status.tracking_dlc == 1 & (status.tracking_animalBehavior == 0 | ...
-            status.tracking_restrictxy == 0 | status.tracking_mazeCoords == 0 | ...
+            status.tracking_restrictxy == 0 | ...
             status.tracking_sessionBehavioralTracking == 0));
         for i = 1:length(basepaths)
             disp(['Processing : ',basepaths{i}])
@@ -50,21 +50,48 @@ switch remedy_check
         end
     case 'kilosort'
         basepaths = status.basepath(status.kilosort == 0);
+        for i = 1:length(basepaths)
+            disp(['Processing : ',basepaths{i}])
+            process_tracking(basepaths{i})
+        end
         f.a = @create_channelmap;
         f.b = @run_ks1;
     case 'sleep_states'
         basepaths = status.basepath(status.sleep_states == 0 & status.lfp == 1);
+        
+        for i = 1:length(basepaths)
+            disp(['Processing : ',basepaths{i}])
+            process_tracking(basepaths{i})
+        end
+        
         f.a = @SleepScoreMaster; % takes lfp in base 0
         f.b = @thetaEpochs;
     case 'lfp'
         basepaths = status.basepath(status.lfp == 0);
         f = @preprocess_session; % if no lfp, preprocessing has likely not been run.
+        
+        for i = 1:length(basepaths)
+            disp(['Processing : ',basepaths{i}])
+            process_tracking(basepaths{i})
+        end
     case 'cell_metrics'
         basepaths = status.basepath(status.kilosort == 1 & status.phyRez == 1 & status.cell_metrics == 0);
-        f = @ProcessCellMetrics;
+        
+        for i = 1:length(basepaths)
+            disp(['Processing : ',basepaths{i}])
+            basename = basenameFromBasepath(basepaths{i});
+            session = loadSession(basepaths{i},basename);
+            ProcessCellMetrics('session',session)
+        end
+        
     case 'ripples'
         basepaths = status.basepath(status.lfp == 1 & status.ripples == 0);
         f = @findRipples;
+        
+        for i = 1:length(basepaths)
+            disp(['Processing : ',basepaths{i}])
+            process_tracking(basepaths{i})
+        end
 end
 
 end
