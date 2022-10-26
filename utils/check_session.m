a687d98c4a4aca7d49353bc6d3b49ce7cc76c347
Laura Berkowitz <leb239@cornell.edu>
2022-10-26 12:45:45 -0400

    adapted for finding sessions without behavior
    This script is used as a general check for whatever needs checking at a given time. Changes dynamically based on need

--- utils/check_session.m
+++ utils/check_session.m
@@ -1,16 +1,36 @@
-data_folder = 'Y:\laura_berkowitz\app_ps1_ephys\data\hpc06';
-folders = dir(data_folder);
-folders = folders(~ismember({folders.name},{'.','..'}),:);
+data_folder = 'Y:\laura_berkowitz\app_ps1_ephys\data\hpc10';
+metadata_path = 'Y:\laura_berkowitz\app_ps1_ephys\behavior\behavior_metadata.csv';
 
+df = compile_sessions(data_folder);
+basepaths = unique([df.basepath{:}])';
 % loop through folders and process those that don't have evidence of
 % processing (in this case chanMap.mat)
-for i = 1:length(folders)
-    basepath = [data_folder,filesep,folders(i).name];
+
+% basepaths(contains(basepaths,'hpc01')) = [];
+for i = 1:length(basepaths)
+%     basepath = df.basepath{i};
+    basepath = basepaths{i};
     basename = basenameFromBasepath(basepath);
-    cd(basepath)
+% load animal behavior file 
+try
+    load(fullfile(basepath,[basename,'.animal.behavior.mat']))
+catch
+    disp('No behavior file found. Generating one now if DLC exists')
+   if ~isempty(dir(fullfile(basepath,'*DLC*.csv'))) 
+           gui_session(basepath)
+
+       % processing the tracking and generates behavior file
+       process_tracking(basepath)
+      
+       % update behavior file with metadata
+       update_behavior_from_metadata(metadata_path,'basepath',basepath);
+   end
+end
+
+if exist('behavior','var')
+    disp('updating metadata')
+    update_behavior_from_metadata(metadata_path,'basepath',basepath);
+end
     
-    session = loadSession(basepath,basename);
-%     gui_session
-    cell_metrics = CellExplorer('basepath',basepath) 
-    
+
 end
