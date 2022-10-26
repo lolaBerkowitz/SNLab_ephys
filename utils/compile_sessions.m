2f5373adfb6df5b4864ee8d9daa64afc7a922f37
Laura Berkowitz <leb239@cornell.edu>
2022-10-26 12:46:43 -0400

    added option to read from csv

--- utils/compile_sessions.m
+++ utils/compile_sessions.m
@@ -3,10 +3,11 @@ function df = compile_sessions(data_folder,varargin)
 % each basepath
 %
 % input: 
-%  data_folder: path to parent directory containing sessions
+%  data_folder: path to parent directory containing sessions or csv to
+%  already compiled sessions.
 %  save_path (optional): path to save df as csv (default, none)
 % ouput: 
-%  table of basepaths from data_folder
+%  table of basepaths,basename from data_folder
 % 
 % LB 2022
 
@@ -17,15 +18,21 @@ p.addParameter('save_path',[],@ischar)
 parse(p,varargin{:});
 save_path = p.Results.save_path;
 
-% use dir to find subdirs and keep only directories 
-folders = dir(data_folder);
-folders = folders(~ismember({folders.name},{'.','..'}),:); % remove ., ..
-folders = folders([folders.isdir],:);
+% if input to csv, load table, else create table from folder contents.
+if contains(data_folder,'.csv')
+    df = readtable(data_folder,'Delimiter','comma');
+else
+    
+    % use dir to find subdirs and keep only directories
+    folders = dir(data_folder);
+    folders = folders(~ismember({folders.name},{'.','..'}),:); % remove ., ..
+    folders = folders([folders.isdir],:);
+    % create table and save basepaths for each subdirectory
+    df = table;
+    df.basepath = cellfun(@(S) fullfile(unique({folders.folder}), S), {folders.name}, 'Uniform', 0)';
+    df.basename = {folders.name}';
+end
 
-% create table and save basepaths for each subdirectory
-df = table; 
-df.basepath = cellfun(@(S) fullfile(unique({folders.folder}), S), {folders.name}, 'Uniform', 0)';
-df.basename = {folders.name}';
 
 % save basepaths if save_path is input
 if ~isempty(save_path)
