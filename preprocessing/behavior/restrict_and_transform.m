04acc2659afeb41f28e5a75566e33675a7f525f9
Laura Berkowitz <leb239@cornell.edu>
2022-10-28 15:08:53 -0400

    changed main sections to local functions, added check to not scale if units are already in cm, added option to save maze coordinates back to maze_coords csv as cm

--- preprocessing/behavior/restrict_and_transform.m
+++ preprocessing/behavior/restrict_and_transform.m
@@ -18,17 +18,60 @@ load(fullfile(basepath,[basename,'.animal.behavior.mat']))
 % load sessions file
 load(fullfile(basepath,[basename,'.session.mat']))
 
+% restrict the coordinates
+behavior = restrict(basepath,basename,behavior);
 
-%% process coordinates in animal behavior file
-start = [];
-stop = [];
+% now scale the coordinates
+[scale_factor_x,scale_factor_y] = scale_coords(behavior,session,basepath,maze_size);
+
+% save maze coords as cm back to maze_coords.csv 
+% loop through coords for each video
+for ep = 1:length(session.behavioralTracking)
+    maze_coords_df = readtable(fullfile(basepath,...
+        [extractBefore(session.behavioralTracking{1,ep}.notes,'.avi'),'_maze_coords.csv']));
+    maze_coords_df.x_cm = maze_coords_df.x/scale_factor_x;
+    maze_coords_df.y_cm = maze_coords_df.y/scale_factor_y;
+    
+    writetable(maze_coords_df,fullfile(basepath,...
+        [extractBefore(session.behavioralTracking{1,ep}.notes,'.avi'),'_maze_coords.csv']));
+end
+
+% save behavior file
+save(fullfile(basepath,[basename,'.animal.behavior.mat']),'behavior')
+end
+
+function behavior = restrict(basepath,basename,behavior)
+% gets index to restrict xy
+
+if ~isempty(dir(fullfile(basepath,[basename,'.restrictxy.mat'])))
+    load(fullfile(basepath,[basename,'.restrictxy.mat']))
+else
+    good_idx = manual_trackerjumps(behavior.timestamps,...
+        behavior.position.x,...
+        behavior.position.y,...
+        start,...
+        stop,...
+        basepath,'darkmode',false);
+end
+% for primary xy coordinates, restrict to boundaries provided by good_idx
+behavior.position.x(~good_idx) = NaN;
+behavior.position.y(~good_idx) = NaN;
+end
+function [scale_factor_x,scale_factor_y] = scale_coords(behavior,session,basepath,maze_size)
+% Scales xy coordinates in behavior and saves back to behavior structure
+
 if isempty(maze_size)
     maze_size = [];
 end
+
+
 x_max = [];
 x_min = [];
 y_max = [];
 y_min = [];
+
+start = [];
+stop = [];
 % loop through epochs to retrieve start/end used in restrictxy below
 for ep = 1:length(session.behavioralTracking)
     epoch = session.behavioralTracking{1,ep}.epoch;
@@ -50,22 +93,6 @@ for ep = 1:length(session.behavioralTracking)
 
 end
 
-% gets index to restrict xy
-
-if ~isempty(dir(fullfile(basepath,[basename,'.restrictxy.mat'])))
-    load(fullfile(basepath,[basename,'.restrictxy.mat']))
-else
-    good_idx = manual_trackerjumps(behavior.timestamps,...
-        behavior.position.x,...
-        behavior.position.y,...
-        start,...
-        stop,...
-        basepath,'darkmode',false);
-end
-% for primary xy coordinates, restrict to boundaries provided by good_idx
-behavior.position.x(~good_idx) = NaN;
-behavior.position.y(~good_idx) = NaN;
-
 % rescale coordinates
 if length(x_max) > 1
     % two mazes average over scale factor
@@ -76,12 +103,18 @@ if length(x_max) > 1
     % TEST HOW THIS WORKS WITH DIFFERENT MAZE SIZES SHOUL BE EQUIVALENT
     scale_factor_x = mean(scale_factor_x);
     scale_factor_y = mean(scale_factor_y);
-
+    
 else
     scale_factor_x = (x_max(1) - x_min(1))/maze_size;
     scale_factor_y = (y_max(1) - y_min(1))/maze_size;
 
 end
+
+if ismember(behavior.position.units,'cm')
+    disp('Coordinates already scaled')
+    return 
+end
+    
 
 coord_names = fieldnames(behavior.position);
 
@@ -95,6 +128,6 @@ for i = find(contains(coord_names,{'y'}))'
     behavior.position.(coord_names{i}) = behavior.position.(coord_names{i})/scale_factor_y;
 end
 behavior.position.units = 'cm';
-
 save(fullfile(basepath,[basename,'.animal.behavior.mat']),'behavior')
+
 end
