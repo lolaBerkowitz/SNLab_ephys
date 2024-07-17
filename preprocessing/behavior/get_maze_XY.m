0478e99fc4b2df1f446017edb605b686b41fc43a
Laura Berkowitz <leb239@cornell.edu>
2024-07-17 14:21:12 -0400

    added pairing for place_preference config option

--- preprocessing/behavior/get_maze_XY.m
+++ preprocessing/behavior/get_maze_XY.m
@@ -215,7 +215,7 @@ elseif contains(name,{'object'})
     config_name = configs{~multi_idx & object_idx & ~open_field_idx};
 elseif contains(name,{'context','open_field','morph','circular_track','pairing_A','pairing_B','linear_track'})
     config_name = configs{~multi_idx & ~object_idx & open_field_idx};
-elseif contains(name,{'pre_test','post_test'})
+elseif contains(name,{'pre_test','post_test','pairing'})
     config_name = configs{place_preference_idx};
 end
 
