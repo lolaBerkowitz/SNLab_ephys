e44d13ee5714301733d56e07f920330e6c426430
Laura Berkowitz <leb239@cornell.edu>
2024-11-26 14:12:42 -0500

    added statement to address ts stored as column or row vectors

--- preprocessing/behavior/manual_trackerjumps.m
+++ preprocessing/behavior/manual_trackerjumps.m
@@ -38,7 +38,11 @@ for i=1:length(StartofRec)
     % use the gui to cut out points
     [~,~,in]=restrictMovement(xtemp,ytemp,restic_dir,darkmode,axis_equal,alpha,add_scatter);
     % save the ts where the tracker error exists
-    savets = [savets; tstemp(in)'];
+    if size(tstemp,2)>1  
+        savets=[savets tstemp(in)];
+    else 
+        savets=[savets; tstemp(in)];
+    end
 end
 % locate the index for each tracker error
 good_idx=ismember(ts,savets);
