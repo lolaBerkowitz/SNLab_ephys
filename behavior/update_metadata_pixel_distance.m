6d1dec70905c0866c808e2303f2f5f5c3780a7ba
Laura Berkowitz <leb239@cornell.edu>
2024-11-26 14:11:46 -0500

    first commit

---
+++ behavior/update_metadata_pixel_distance.m
@@ -0,0 +1,26 @@
+function update_metadata_pixel_distance(data_path)
+
+df = readtable(data_path,'Delimiter','comma'); 
+
+for i = 1:length(df.basepath)
+    
+    if ~isnan(df.pixel_distance(i))
+        continue
+    end
+    
+    if ismember(df.vidname{i},{'na','MISSING'})
+        df.pixel_distance(i) = nan;
+    else
+        video_path = fullfile(df.basepath{i},[df.vidname{i},'.avi']);
+        maze_size = df.maze_width_cm(i);
+        
+        pixel_distance = tracking.maze_distance_gui(video_path,maze_size,300);
+
+        df.pixel_distance(i) = pixel_distance; 
+        
+        writetable(df,df_path)
+    end
+      
+end
+
+end
