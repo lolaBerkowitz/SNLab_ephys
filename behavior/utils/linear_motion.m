74c55b6d4302f8807ea28318153fbb85f921a264
Laura Berkowitz <leb239@cornell.edu>
2025-03-24 23:04:02 -0400

    replaced linear_velocity with FMA code.

--- behavior/utils/linear_motion.m
+++ behavior/utils/linear_motion.m
@@ -1,9 +1,10 @@
-function [velocity, acceleration,distance_vector] = linear_motion(x,y,fr,smooth_factor)
+function [velocity, acceleration, distance_vector] = linear_motion(x,y,t,fr)
 % computes linear velocity and acceleration from position coordinates
 % inputs: 
 %   x: position vector of x-coordinates of length n
-%   y: position vecotr of y-coordinates of length n
-%   smooth_factor: time in seconds to smooth over (e.g .100 for 100ms)  
+%   y: position vector of y-coordinates of length n
+%   t: position vectpr of timestamps of length n
+%   fr : video frame rate in Hz 
 % output:
 %   velocity: n-1 vector of velocity
 %   acceleration: second derivative of position data using matlab gradient
@@ -15,17 +16,8 @@ sqrYDiff = (diff(y)).^2;
 
 %distance formula
 distance_vector = sqrt(sqrXDiff + sqrYDiff);
-velocity = (distance_vector)*fr; %instanteous velocity
+velocity = LinearVelocity([t, x, y]);
+velocity = velocity(:,2);
 acceleration = gradient(velocity,1/fr); %instanteous acceleration
 
-frames_to_smooth = nearest_odd(fr*smooth_factor);
-velocity = sgolayfilt(velocity,3,frames_to_smooth);
-
-% set negative values to nan
-velocity(velocity < 0) = nan;
-end
-
-% to set the number of frames to smooth - must be odd for Savitzky-Golay
-function y = nearest_odd(x)
-y = 2*floor(x/2)+1;
 end
