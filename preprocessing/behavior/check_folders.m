e944e2c496a91bfbef35f297e6f2d79fd30804e0
Jean-Luc Shimizu <jns99@cornell.edu>
2025-04-03 11:09:04 -0400

    fix for when video files don't exist

--- preprocessing/behavior/check_folders.m
+++ preprocessing/behavior/check_folders.m
@@ -1,20 +1,26 @@
 function check = check_folders(basepath)
 % see if DLC files are present for each video
 
-% find all .avi videos and dlc files in basepath
-video_files = dir(fullfile(basepath,'*.avi'));
-video_files = video_files(~cellfun(@(x) ismember(x(1,2),'._'), {video_files.name}));
-video_files = extractBefore({video_files.name},'.avi');
+    % find all .avi videos and dlc files in basepath
+    video_files = dir(fullfile(basepath,'*.avi'));
+    video_files = video_files(~cellfun(@(x) ismember(x(1,2),'._'), {video_files.name}));
+    video_files = extractBefore({video_files.name},'.avi');
 
-dlc_files = get_dlc_files_in_basepath(basepath);
-dlc_files = extractBefore({dlc_files.name},'DLC');
+    if isempty(video_files)
+        check = false;
+        return
 
-% see if each video has a DLC file
-if sum(contains(video_files,dlc_files)) == length(video_files)
-    check = true;
-else
-    check = false;
-end
+    end
+    
+    dlc_files = get_dlc_files_in_basepath(basepath);
+    dlc_files = extractBefore({dlc_files.name},'DLC');
+
+    % see if each video has a DLC file
+    if sum(contains(video_files,dlc_files)) == length(video_files)
+        check = true;
+    else
+        check = false;
+    end
 
 
 end
