8be553e2ae0a9d16f53f0c8ab0884f8654cf114e
Laura Berkowitz <leb239@cornell.edu>
2025-07-26 13:49:22 -0400

    Harded coded options for context configs for updating trials
    Again we should optimized this by loading from metadata_csv

---
+++ preprocessing/behavior/context_trial_configs.m
@@ -0,0 +1,37 @@
+
+% AABB
+behavior.trialsID{1, 1}   = 'A';
+behavior.trialsID{2, 1}   = 'A';
+behavior.trialsID{3, 1}   = 'B';
+behavior.trialsID{4, 1}   = 'B';
+
+%BBAA
+behavior.trialsID{1, 1}   = 'B';
+behavior.trialsID{2, 1}   = 'B';
+behavior.trialsID{3, 1}   = 'A';
+behavior.trialsID{4, 1}   = 'A';
+
+%BABA
+behavior.trialsID{1, 1}   = 'B';
+behavior.trialsID{2, 1}   = 'A';
+behavior.trialsID{3, 1}   = 'B';
+behavior.trialsID{4, 1}   = 'A';
+
+%ABAB
+behavior.trialsID{1, 1}   = 'A';
+behavior.trialsID{2, 1}   = 'B';
+behavior.trialsID{3, 1}   = 'A';
+behavior.trialsID{4, 1}   = 'B';
+
+%BAAB
+behavior.trialsID{1, 1}   = 'B';
+behavior.trialsID{2, 1}   = 'A';
+behavior.trialsID{3, 1}   = 'A';
+behavior.trialsID{4, 1}   = 'B';
+
+%ABBA
+%BABA
+behavior.trialsID{1, 1}   = 'A';
+behavior.trialsID{2, 1}   = 'B';
+behavior.trialsID{3, 1}   = 'B';
+behavior.trialsID{4, 1}   = 'A';
