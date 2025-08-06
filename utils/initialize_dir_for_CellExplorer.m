b395255fb7eafa488e3ad4eb42a8029aee8f1de1
Laura Berkowitz <leb239@cornell.edu>
2025-08-06 12:56:11 -0400

    use compile_sessions for greater versatility in loading (path or csv),  will now skip files already created

--- utils/initialize_dir_for_CellExplorer.m
+++ utils/initialize_dir_for_CellExplorer.m
@@ -2,16 +2,20 @@ function initialize_dir_for_CellExplorer(subject_path)
 %% For a subject directory, this function will initalize all basepaths within the subject folder with basename.session.mat files. 
 
 % Use dir to get sessions in subject folder 
-session_list = dir(subject_path);
-session_list = session_list(~ismember({session_list.name},{'.','..'}),:); 
-session_list = session_list(~cellfun(@(x) contains(x,'DS_Store'),{session_list.name})); 
+ session_list = compile_sessions(subject_path);
+
 % loop through session and create a basename.session file
-for i = 1:length(session_list)
+for i = 1:length(session_list.basepath)
     
-    basepath = fullfile(subject_path,session_list(i).name); 
-    basename = basenameFromBasepath(basepath);
-    session = sessionTemplate(basepath,'basename',basename);
-    save(fullfile(basepath,[basename, '.session.mat']),'session');
+    basepath = session_list.basepath{i}; 
+    basename = session_list.basename{i};
+    
+    if ~exist(fullfile(basepath,[basename, '.session.mat']),'file')
+        session = sessionTemplate(basepath,'basename',basename);
+        save(fullfile(basepath,[basename, '.session.mat']),'session');
+    else
+        disp('session file already created, skipping basepath')
+    end
     
 end
 
