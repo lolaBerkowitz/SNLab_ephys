0961ac01a0f625a2ead0aed415353511b0839f19
Laura Berkowitz <leb239@cornell.edu>
2025-12-10 11:20:21 -0500

    Improve IED removal from ripples detection
    Refactored function to load updated IED event file, handle missing files, and remove flagged IED events. Updated interval bounds for IED overlap detection and improved logic for flagging ripples overlapping with IEDs.

--- lfp/remove_IEDs_from_ripples.m
+++ lfp/remove_IEDs_from_ripples.m
@@ -1,16 +1,39 @@
 function remove_IEDs_from_ripples(basepath)
-
-basename = basenameFromBasepath(basepath);
 % Load ripples, if ripple event overlaps with IED then define as
 % IED ( Gelinas et al 2016)
-load(fullfile(basepath,[basename,'.ripples.events.mat']))
-load(fullfile(basepath,[basename,'.interictal_spikes_.events.mat']))
+% LB 2023
+
+basename = basenameFromBasepath(basepath);
+
+rip_file = fullfile(basepath,[basename,'.ripples.events.mat']);
+ied_file = fullfile(basepath,[basename,'.interictal_spikes_offline.events.mat']);
+
+% Load ripples and ieds
+if isfile(rip_file) & isfile(ied_file)
+    load(fullfile(basepath,[basename,'.ripples.events.mat']),'ripples')
+    load(fullfile(basepath,[basename,'.interictal_spikes_offline.events.mat']),'interictal_spikes_offline')
+else
+    disp('Missing Ripples or IEDs, skipping')
+    return
+end
+
+% remove flagged events (sometimes these are ripples
+if isfield(interictal_spikes_offline,'flagged')
+    interictal_spikes_offline.peaks(interictal_spikes_offline.flagged) = [];
+end
+
+% set interval bounds - anything 10ms before IED peak and 200ms after 
+bounds = [interictal_spikes_offline.peaks' - 0.010, interictal_spikes_offline.peaks' + 0.2];
 
 % Find IEDs that were also detected as physiological ripples
-[status,interval,~] = InIntervals(interictal_spikes_.peaks,ripples.timestamps);
+[status,~,~] = InIntervals(ripples.peaks,bounds);
 
 % Set those phyviological ripples as flagged
-ripples.flagged = interval(status);
+if isfield(ripples,'flagged')
+    ripples.flagged = unique(sort([ripples.flagged; find(status)]));
+else
+    ripples.flagged = find(status);
+end
 
 save(fullfile(basepath,[basename '.ripples.events.mat']),'ripples')
 end
